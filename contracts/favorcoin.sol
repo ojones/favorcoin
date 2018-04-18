ac8630bbe5cd296dfef6cc764245f234029c360d
ojones <wakeupoj@gmail.com>
2018-04-18 11:08:12 -0700

    more tests

--- contracts/favorcoin.sol
+++ contracts/favorcoin.sol
@@ -2,6 +2,9 @@ pragma solidity ^0.4.17;
 
 import "./safemath.sol";
 
+// This is just an experimental work in progres.
+// Learn more at: https://collabcoins.com
+
 contract FavorCoin {
 
   using SafeMath for uint256;
@@ -9,28 +12,6 @@ contract FavorCoin {
   event NewFriendGroup(uint _friendGroup, address _owner);
   event NewFriend(uint _friendGroup, address _friend);
   event NewFavor(uint _friendGroup, address _from, address _to);
-
-  function accountOf(address _owner) public view returns (FavorAcct) {
-    return accounts[_owner];
-  }
-
-  function friends() public view returns (address[]) {
-    return _friendsOf(msg.sender);
-  }
-
-  function _friendsOf(address _owner) private view returns (address[]) {
-    uint friendGroup = friendToGroup[_owner];
-    return groupToFriends[friendGroup];
-  }
-
-  function groupMembers(uint friendGroup) public view returns (uint) {
-    return groupToFriends[friendGroup].length;
-  }
-
-  modifier groupOwner() {
-    require(ownerToGroup[msg.sender] != 0);
-    _;
-  }
 
   struct FavorAcct {
     bool exists;
@@ -55,7 +36,7 @@ contract FavorCoin {
   }
 
   function addFriend(address _friend) public groupOwner {
-    if (friendToGroup[msg.sender] == 0) { revert(); }
+    require(friendToGroup[msg.sender] != 0);
     uint friendGroup = friendToGroup[msg.sender];
     friendToGroup[_friend] = friendGroup;
     _newFavorAcct(_friend);
@@ -63,7 +44,6 @@ contract FavorCoin {
   }
 
   function _newFavorAcct(address _owner) private {
-    // if (!accounts[_owner].exists) { revert(); }
     require(!accounts[_owner].exists);
     uint friendGroup = friendToGroup[_owner];
     uint favorBalance = 1;
@@ -72,12 +52,13 @@ contract FavorCoin {
     groupToFriends[friendGroup].push(_owner);
   }
 
-  function giveFavorCoin(address _to) public {
-    if (friendToGroup[msg.sender] == 0) { revert(); }
-    if (friendToGroup[_to] == 0) { revert(); }
-    if (friendToGroup[msg.sender] != friendToGroup[_to]) { revert(); }
-    if (accounts[msg.sender].favorBalance > 0) { revert(); }
+  function giveFavorCoin(address _to) public returns (uint) {
+    require(friendToGroup[msg.sender] != 0);
+    require(friendToGroup[_to] != 0);
+    require(friendToGroup[msg.sender] == friendToGroup[_to]);
+    require(accounts[msg.sender].favorBalance != 0);
     _giveFavorCoin(msg.sender, _to);
+    return accounts[msg.sender].favorBalance;
   }
 
   function _giveFavorCoin(address _from, address _to) private {
@@ -85,6 +66,44 @@ contract FavorCoin {
     accounts[_from].favorBalance--;
     accounts[_to].reputationBalance++;
     NewFavor(friendGroup, _from, _to);
+  }
+
+  function accountOf(address _owner) public view returns (FavorAcct) {
+    require(accounts[_owner].exists);
+    return accounts[_owner];
+  }
+
+  function groupOf(address _owner) public view returns (uint) {
+    require(accounts[_owner].exists);
+    return accounts[_owner].friendGroup;
+  }
+
+  function favorBalanceOf(address _owner) public view returns (uint) {
+    require(accounts[_owner].exists);
+    return accounts[_owner].favorBalance;
+  }
+
+  function reputationBalanceOf(address _owner) public view returns (uint) {
+    require(accounts[_owner].exists);
+    return accounts[_owner].reputationBalance;
+  }
+
+  function friends() public view returns (address[]) {
+    return _friendsOf(msg.sender);
+  }
+
+  function _friendsOf(address _owner) private view returns (address[]) {
+    uint friendGroup = friendToGroup[_owner];
+    return groupToFriends[friendGroup];
+  }
+
+  function groupMembers(uint friendGroup) public view returns (uint) {
+    return groupToFriends[friendGroup].length;
+  }
+
+  modifier groupOwner() {
+    require(ownerToGroup[msg.sender] != 0);
+    _;
   }
 
 }
--- test/favorcoin.test.js
+++ test/favorcoin.test.js
@@ -5,7 +5,7 @@ contract('FavorCoin test', function(accounts) {
   it("should create the first friend group", async () => {
     let instance = await FavorCoin.deployed();
     let result = await instance.createFriendGroup({from: accounts[0]});
-    const groupNum = result.logs[0].args._friendGroup.s;
+    const groupNum = result.logs[0].args._friendGroup.c[0];
     assert.equal(groupNum, 1, "wrong first friend group");
   })
 
@@ -13,10 +13,46 @@ contract('FavorCoin test', function(accounts) {
     let instance = await FavorCoin.deployed();
     let result1 = await instance.addFriend(accounts[1], {from: accounts[0]});
     const groupNum = result1.logs[0].args._friendGroup.c[0];
-    const fiend = result1.logs[0].args._friend;
-    assert.equal(fiend, accounts[1], "wrong friend");
     let result2 = await instance.groupMembers.call(groupNum, {from: accounts[0]});
-    assert.equal(result2.c[0], 2, "both members not found in group");
+    const numMembers = result2.c[0]
+    assert.equal(numMembers, 2, "both members not found in group");
+  })
+
+  it("should log friend in event when adding friend", async () => {
+    let instance = await FavorCoin.deployed();
+    let result = await instance.addFriend(accounts[2], {from: accounts[0]});
+    const groupNum = result.logs[0].args._friendGroup.c[0];
+    const fiend = result.logs[0].args._friend;
+    assert.equal(fiend, accounts[2], "wrong friend logged");
+  })
+
+  it("new accounts should have reputation balance of 0", async () => {
+    let instance = await FavorCoin.deployed();
+    let result = await instance.reputationBalanceOf.call(accounts[2], {from: accounts[0]});
+    const balance = result.c[0]
+    assert.equal(balance, 0, "wrong reputation balance for new account");
+  })
+
+  it("new accounts should have favor balance of 1", async () => {
+    let instance = await FavorCoin.deployed();
+    let result = await instance.favorBalanceOf.call(accounts[2], {from: accounts[0]});
+    const balance = result.c[0]
+    assert.equal(balance, 1, "wrong favor balance for new account");
+  })
+
+  it("should have favor balance of 0 after giving a favor coin", async () => {
+    let instance = await FavorCoin.deployed();
+    let result1 = await instance.giveFavorCoin(accounts[1], {from: accounts[0]});
+    let result2 = await instance.favorBalanceOf.call(accounts[0], {from: accounts[0]});
+    const balance = result2.c[0]
+    assert.equal(balance, 0, "wrong favor balance for new account");
+  })
+
+  it("should log favor in event when giving favor", async () => {
+    let instance = await FavorCoin.deployed();
+    let result = await instance.giveFavorCoin(accounts[0], {from: accounts[2]});
+    const favorTo = result.logs[0].args._to;
+    assert.equal(favorTo, accounts[0], "wrong favor to logged");
   })
 
 });
